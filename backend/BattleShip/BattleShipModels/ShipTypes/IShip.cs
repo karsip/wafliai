e6df2217b156f8e1564dc13cb315ce9efe2f814a
IgnasBarakauskas <b.ignas05@gmail.com>
2020-10-01 12:52:59 +0300

    Ship factory pattern demo

---
+++ backend/BattleShip/BattleShipModels/ShipTypes/IShip.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleShipModels.ShipTypes
+{
+    abstract class IShip
+    {
+        public abstract string ShipType { get; }
+        public abstract string Visuals { get; set; }
+        public abstract int Speed { get; set; }
+
+    }
+}
